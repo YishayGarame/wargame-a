3cf4391616d6df6b2d6d674ee2d8d387bcf1a9c9
YishayGarame <yishatgr@gmail.com>
2020-05-25 18:34:58 +0300

    before upload3

--- Board.cpp
+++ Board.cpp
@@ -20,16 +20,16 @@ void Board::move(uint player_number, std::pair<int, int> source, MoveDIR directi
 
 bool Board::has_soldiers(uint player_number) const
 {
-    for (int i = 0; i < board.size(); i++)
-    {
-        for (int j = 0; j < board[i].size(); i++)
-        {
-            if (board[i][j] != nullptr)
-            {
-                if (board[i][j]->player_number == player_number)
-                    return true;
-            }
-        }
-    }
+    // for (int i = 0; i < board.size(); i++)
+    // {
+    //     for (int j = 0; j < board[i].size(); i++)
+    //     {
+    //         if (board[i][j] != nullptr)
+    //         {
+    //             if (board[i][j]->player_number == player_number)
+    //                 return true;
+    //         }
+    //     }
+    // }
     return false;
 }
